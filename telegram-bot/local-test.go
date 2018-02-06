bf32c48716839dc38f256653c9748c17bf16f771
Laurynas Usas <demox@Laurynass-MacBook-Pro.local>
2018-02-06 17:46:19 +0200

    telegram-bot GetInfo example working from local server.

---
+++ telegram-bot/local-test.go
@@ -0,0 +1,79 @@
+package main
+
+// Simple echo bot, that responses with the same message
+
+import (
+	"flag"
+	"log"
+
+	"github.com/bot-api/telegram"
+	"github.com/bot-api/telegram/telebot"
+	"golang.org/x/net/context"
+)
+
+func main() {
+	token := flag.String("token", "", "telegram bot token")
+	debug := flag.Bool("debug", false, "show debug information")
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token flag is required")
+	}
+
+	api := telegram.New(*token)
+	api.Debug(*debug)
+	bot := telebot.NewWithAPI(api)
+	bot.Use(telebot.Recover()) // recover if handler panic
+
+	netCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	bot.HandleFunc(func(ctx context.Context) error {
+		update := telebot.GetUpdate(ctx) // take update from context
+		if update.Message == nil {
+			return nil
+		}
+		api := telebot.GetAPI(ctx) // take api from context
+		msg := telegram.CloneMessage(update.Message, nil)
+		_, err := api.Send(ctx, msg)
+		return err
+
+	})
+	err := bot.Serve(netCtx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+// }
+
+// func LocalTestServer() {
+// 	http.HandleFunc("/", handler)
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+// func main() {
+// 	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	bot.Debug = true
+
+// 	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+// 	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://www.google.com:8443/"+bot.Token, "cert.pem"))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	updates := bot.ListenForWebhook("/" + bot.Token)
+// 	go http.ListenAndServeTLS("localhost:8080", "cert.pem", "key.pem", nil)
+
+// 	for update := range updates {
+// 		log.Printf("%+v\n", update)
+// 	}
+// }
